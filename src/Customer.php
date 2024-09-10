9974e8f0274b4818a37f434b9966d9764dcb3946
rezafikkri <fikkri.reza@gmail.com>
2024-09-10 09:59:38 +0700

    feat: add default parameter in sayHello method and property

--- src/Customer.php
+++ src/Customer.php
@@ -4,12 +4,12 @@ namespace RezaFikkri\BelajarComposerLibrary;
 
 class Customer
 {
-    public function __construct(private string $name)
+    public function __construct(private string $name = 'RezaFikkri')
     {
         
     }
 
-    public function sayHello(string $name): string
+    public function sayHello(string $name = 'Guest'): string
     {
         return "Hello $name, my name is $this->name";
     }
