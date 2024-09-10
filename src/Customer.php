73feea673badb86b2ccf92408a0aecfa24ca7cb1
rezafikkri <fikkri.reza@gmail.com>
2024-09-10 10:14:32 +0700

    feat: add address

--- src/Customer.php
+++ src/Customer.php
@@ -4,13 +4,20 @@ namespace RezaFikkri\BelajarComposerLibrary;
 
 class Customer
 {
-    public function __construct(private string $name = 'RezaFikkri')
-    {
+    public function __construct(
+        private string $name = 'RezaFikkri',
+        private string $address,
+    ) {
         
     }
 
     public function sayHello(string $name = 'Guest'): string
     {
         return "Hello $name, my name is $this->name";
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
     }
 }
