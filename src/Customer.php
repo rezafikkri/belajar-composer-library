775c47f3854dcc7c251080414065230dcfc23c29
rezafikkri <fikkri.reza@gmail.com>
2024-09-10 09:30:57 +0700

    Initial commit

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RezaFikkri\BelajarComposerLibrary;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+        
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, my name is $this->name";
+    }
+}
